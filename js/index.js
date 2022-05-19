7cdd042b7a1e25c5b5204e3c62f8f27040f12053
RicScomputer <3476952607@qq.com>
2022-05-19 17:38:47 +0800

    ricardo

---
+++ js/index.js
@@ -0,0 +1,9 @@
+let p = document.querySelector(".firstpage p");
+setTimeout(function(){
+    p.style.opacity=1;
+},2000);
+
+let nameContainer = document.querySelector('.name-container');
+setTimeout(() => {
+    nameContainer.style.opacity=1;
+}, 3000);
